838d70b018f0ad7f1e75a9097cce6c9dc97b7af0
suraj kumar <surajk.avology@gmail.com>
2026-06-13 00:36:00 +0530

    update contact

--- js/function.js
+++ js/function.js
@@ -315,43 +315,43 @@
 	});
 
 	/* Contact form validation */
-	var $contactform = $("#contactForm");
-	$contactform.validator({focus: false}).on("submit", function (event) {
-		if (!event.isDefaultPrevented()) {
-			event.preventDefault();
-			submitForm();
-		}
-	});
+	// var $contactform = $("#contactForm");
+	// $contactform.validator({focus: false}).on("submit", function (event) {
+	// 	if (!event.isDefaultPrevented()) {
+	// 		event.preventDefault();
+	// 		submitForm();
+	// 	}
+	// });
 
-	function submitForm(){
-		/* Ajax call to submit form */
-		$.ajax({
-			type: "POST",
-			url: "form-process.php",
-			data: $contactform.serialize(),
-			success : function(text){
-				if (text === "success"){
-					formSuccess();
-				} else {
-					submitMSG(false,text);
-				}
-			}
-		});
-	}
+	// function submitForm(){
+	// 	/* Ajax call to submit form */
+	// 	$.ajax({
+	// 		type: "POST",
+	// 		url: "form-process.php",
+	// 		data: $contactform.serialize(),
+	// 		success : function(text){
+	// 			if (text === "success"){
+	// 				formSuccess();
+	// 			} else {
+	// 				submitMSG(false,text);
+	// 			}
+	// 		}
+	// 	});
+	// }
 
-	function formSuccess(){
-		$contactform[0].reset();
-		submitMSG(true, "Message Sent Successfully!")
-	}
+	// function formSuccess(){
+	// 	$contactform[0].reset();
+	// 	submitMSG(true, "Message Sent Successfully!")
+	// }
 
-	function submitMSG(valid, msg){
-		if(valid){
-			var msgClasses = "h4 text-success";
-		} else {
-			var msgClasses = "h4 text-danger";
-		}
-		$("#formsubmit").removeClass().addClass(msgClasses).text(msg);
-	}
+	// function submitMSG(valid, msg){
+	// 	if(valid){
+	// 		var msgClasses = "h4 text-success";
+	// 	} else {
+	// 		var msgClasses = "h4 text-danger";
+	// 	}
+	// 	$("#formsubmit").removeClass().addClass(msgClasses).text(msg);
+	// }
 	/* Contact form validation end */
 
 	/* Appointment form validation */
